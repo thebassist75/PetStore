c1643ac3a57725375ae5cf928de51cc833c2af4a
Adam Yates <beethoven123456@yahoo.com>
2016-10-19 16:31:09 -0700

    PetStore commit

---
+++ petstore.js
@@ -0,0 +1,12 @@
+
+function setColor1(){
+  document.getElementById("bkCl1").style.backgroundColor = "black";
+}
+
+function setColor2(){
+  document.getElementById("bkCl2").style.backgroundColor = "green";
+}
+
+function setColor3(){
+  document.getElementById("bkCl3").style.backgroundColor = "yellow";
+}
